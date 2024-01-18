37ab0b6d7b53cfa7fa34f215e8597a36834a9cd4
Leonardo <leondev91@gmail.com>
2024-01-17 19:45:32 -0500

    Variables

---
+++ 001-FundamentosDeProgramacion/004-JavaScript/01-De-Cero-a-Junior/capitulo-01/script.js
@@ -0,0 +1,37 @@
+alert("Hola mundo")
+
+// Variables
+recipiente = "papel";
+
+// Declaración de variable
+// var, let, const - tienen que ver con el scope
+// const se debe declarar e inicializar al mismo tiempo
+
+// Declaración
+let numero;
+
+// Casos especiales de tipos de datos: undefined, null, nan
+
+// Undefined: la variable existe pero no tiene valor
+alert(numero);
+
+// Null: valor arbitrario, intencional, dado por el programador
+numero = null;
+alert(numero);
+
+// NaN: el resultado de la operación no es un número
+let num = 5;
+let nom = "Pedro";
+alert(num * nom);
+
+// Inicialización
+numero = 20;
+alert(numero);
+
+const nombre = "Juan";
+alert(nombre);
+
+// Tipos de variables
+string = "cadena de texto";
+number = 19;
+boolean = true;
