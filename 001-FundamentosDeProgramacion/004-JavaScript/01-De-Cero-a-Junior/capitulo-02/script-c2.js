c707f9d09300494885259f337cdb81d8eb9f0527
Leonardo <leondev91@gmail.com>
2024-01-19 05:10:39 -0500

    Bucles

--- 001-FundamentosDeProgramacion/004-JavaScript/01-De-Cero-a-Junior/capitulo-02/script-c2.js
+++ 001-FundamentosDeProgramacion/004-JavaScript/01-De-Cero-a-Junior/capitulo-02/script-c2.js
@@ -20,3 +20,84 @@ let pc = {
 
 let componente = pc["procesador"];
 document.write(`<p>${componente}</p>`);
+document.write(`<hr>`);
+
+// Bucles
+
+// while
+// Primero se pregunta, luego se ejecuta
+// bucle indeterminado: "no sabemos" cuantos ciclos va a realizar
+let numero = 0;
+
+while (numero < 10) {
+  numero++;
+  document.write(`<p>${numero}</p>`);
+}
+
+document.write(`<hr>`);
+
+// do-while
+// Primero se ejecuta, luego se pregunta
+// bucle indeterminado: "no sabemos" cuantos ciclos va a realizar
+numero = 0;
+
+do {
+  numero++;
+  document.write(`<p>${numero}</p>`);
+}
+while(numero <5);
+
+do{
+  numero++; // numero incrementa a 6
+  document.write(`<p>${numero}</p>`); // se muestra numero en pantalla
+}
+while(numero<5);
+
+document.write(`<hr>`);
+
+// for
+// bucle determinado: sabemos cuantos ciclos va a realizar
+// declaramos, inicializamos, comparamos, iteramos
+for(let i = 0; i < 10; i++) {
+  document.write(`<p>${i}</p>`);
+}
+
+let i = 6;
+for(i; i >= 0; i--) {
+  document.write(`<p>${i}</p>`);
+}
+
+document.write(`<hr>`);
+
+// for in
+// devuelve el índice
+let animales = ["gato","perro","loro","hamster","tortuga"];
+
+for(animal in animales) {
+  document.write(`<p>${animal} - ${animales[animal]}</p>`);
+}
+
+document.write(`<br>`);
+
+// for of
+// devuelve el valor
+for(animal of animales) {
+  document.write(`<p>${animal}</p>`);
+}
+
+// break, continue, label
+// label
+arr1 = ["maria","josefa","roberta"];
+arr2 = ["pedro","marcelo",arr1,"josefina"];
+
+forLabel:
+for(let array in arr2) {
+  if(array == 2) {
+    for(let array of arr1) {
+      break forLabel;
+      document.write(array + "<br>");
+    }
+  }else{
+    document.write(arr2[array] + "<br>");
+  }
+}
