418f9c4f150b9b9e8859b0c7670c8fc3e97210bc
Leonardo <leondev91@gmail.com>
2024-01-26 17:44:56 -0500

    DOM (Document Object Model)

---
+++ 001-FundamentosDeProgramacion/004-JavaScript/01-De-Cero-a-Junior/capitulo-06/script-c6.js
@@ -0,0 +1,13 @@
+// DOM (Document Object Model)
+
+// Nodo: un nodo en el DOM es cualquier etiqueta del cuerpo
+// como un párrafo, el mismo body o incluso las etiquetas de una lista
+// a veces suelen ser otro tipo de elementos
+
+// una etiqueta crea un nodo, pero no todos los nodos son etiquetas
+
+// document: es el nodo raíz, a partir del cual derivan el resto de nodos
+// element: nodos difinidos por etiquetas html
+// text: el texto dentro de un nodo element, se considera un nuevo nodo hijo de tipo texto
+// attribute: los atributos de las etiquetas definen nodos (en JS no los veremos como nodos, sino como información asociada al nodo de tipo element)
+// comentarios y otros: los comentarios y otros elementos como las declaraciones doctype en la cabecera de los elementos html generan nodos
