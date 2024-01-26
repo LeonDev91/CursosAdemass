be2cd04267d74e9fac963360f7e4e01b8f691680
Leonardo <leondev91@gmail.com>
2024-01-25 19:28:13 -0500

    Métodos de consola: conteo, agrupación, temporización

--- 001-FundamentosDeProgramacion/004-JavaScript/01-De-Cero-a-Junior/capitulo-05/script-c5.js
+++ 001-FundamentosDeProgramacion/004-JavaScript/01-De-Cero-a-Junior/capitulo-05/script-c5.js
@@ -1,5 +1,7 @@
 // CONSOLE
 
+// Métodos de registro
+
 // assert: aparece un mensaje de error en la consola si la afirmación es falsa.
 // si la afirmación es verdadera, no aparece nada
 console.assert(5>3);
@@ -26,3 +28,49 @@ console.warn("Mensaje de advertencia");
 
 // dir: despliega una lista interactiva de las propiedades del objeto JavaScript especificado
 console.dir([1,3,"salud",4,7,0.9,'e']);
+
+
+// Métodos de conteo
+
+// count: registra el número de veces que se llama a count()
+// esta función toma como argumento opcional una etiqueta
+console.count();
+console.count();
+console.count();
+console.count();
+console.count();
+
+// countReset: resetea el contador console.count()
+console.countReset();
+console.count();
+
+
+// Métodos de agrupación
+
+// group: crea un nuevo grupo en línea en el registro de la consola
+// groupEnd: remueve un grupo en línea en el registro de la consola
+// groupCollapsed: crea un grupo en línea pero contraido
+console.group();
+console.group("Segundo nivel");
+console.log("Hola 1");
+console.log("Hola 2");
+console.groupEnd();
+console.groupCollapsed("nuevo segundo nivel");
+console.log("Hola 3");
+console.log("Hola 4");
+console.groupEnd();
+console.groupEnd();
+
+
+// Métodos de temporización
+
+// time: inicia un temporizador
+// timeLog: detiene un temporizador
+// timeEnd: registra el valor actual del temporizador
+console.time();
+console.timeLog();
+console.timeLog();
+console.timeLog();
+console.timeLog();
+console.timeLog();
+console.timeEnd();
