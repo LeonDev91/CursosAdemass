eb37bed10ef0d8a6b09f3c040ac38a4f077bd8fa
Leonardo <leondev91@gmail.com>
2024-02-26 20:47:57 -0500

    alert y print

--- 001-FundamentosDeProgramacion/004-JavaScript/02-De-Junior-a-Mid/capitulo-07/script-c7.js
+++ 001-FundamentosDeProgramacion/004-JavaScript/02-De-Junior-a-Mid/capitulo-07/script-c7.js
@@ -1,16 +1,22 @@
 // window
 
-// open()
+// open() - carga un recurso en el contexto de un nuevo navegador o uno que ya existe
 let youtubeURL = "https://youtube.com";
 let ventanaYoutube = window.open(youtubeURL);
 
-// close()
+// close() - cierra la ventana actual, o la ventana en la que se llamó
 //wYoutube.close();
 
-// closed - verifica si la ventana está cerrada o no
+// closed - indica si la ventana referenciada está cerrada o no
 console.log(ventanaYoutube.closed);
 
-// stop() - para la carga de la ventana
+// stop() - detiene la carga de recursos en el contexto de navegación actual
 let facebookURL = "https://facebook.com";
 let ventanaFacebook = window.open(facebookURL);
 ventanaFacebook.stop();
+
+// alert() - muestra un cuadro de alerta con el contenido especificado y un botón aceptar
+window.alert("Mensaje de prueba - window.alert()"); // o simplemente alert()
+
+// print() - abre el cuadro de diálogo imprimir para imprimir el documento actual
+//window.print(); // o simplemente print()
